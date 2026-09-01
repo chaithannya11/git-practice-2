31ec29458c98dd69ac6dc269096c0c8ff90fce32
chaithu <saichaithannya@gmail.com>
2026-09-01 16:56:37 +0530

    handle missing active user status

--- src/users.py
+++ src/users.py
@@ -2,7 +2,7 @@ def get_user_name(user):
     return user["name"]
 
 def is_active_user(user):
-    return user["active"]
+    return user.get("active",false)
 
 def get_user_email(user):
     return user["email"]
